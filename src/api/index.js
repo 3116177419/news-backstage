a6a209a77d47c83b20bd54c09cfee51e555f5377
xiaozhang13 <3116199419@qq.com>
2022-08-26 10:05:25 +0800

    新闻后台管理项目

---
+++ src/api/index.js
@@ -0,0 +1,222 @@
+import request from '@/utils/request'
+
+/**
+ * 注册接口
+ * @param {*} param0 { username: 用户名, password: 密码 }
+ * @returns Promise对象
+ */
+export const registerAPI = ({ username, password, repassword }) => {
+  return request({
+    url: '/api/reg',
+    method: 'post',
+    data: {
+      username,
+      password,
+      repassword
+    }
+  })
+}
+
+/**
+ * 登录接口
+ * @param {*} param0 { username: 用户名, password: 密码 }
+ * @returns Promise对象
+ */
+export const loginAPI = ({ username, password }) => {
+  return request({
+    url: '/api/login',
+    method: 'post',
+    data: {
+      username,
+      password
+    }
+  })
+}
+
+/**
+ * 获取-用户基础资料
+ * @returns Promise对象
+ */
+export const getUserInfoAPI = () => {
+  return request({
+    url: '/my/userinfo'
+  })
+}
+
+/**
+ * 获取-侧边栏菜单数据
+ * @returns Promise对象
+ */
+export const getMenusAPI = () => {
+  return request({
+    url: '/my/menus'
+  })
+}
+
+/**
+ * 更新-用户基本资料
+ * @param {*} param0 { id: 用户id, email: 用户邮箱, nickname: 用户昵称, user_pic: 用户头像地址, username: 用户名 }
+ * @returns Promise对象
+ */
+export const updateUserInfoAPI = ({ id, email, nickname, user_pic, username }) => {
+  return request({
+    url: '/my/userinfo',
+    method: 'PUT',
+    data: {
+      id,
+      email,
+      nickname,
+      user_pic,
+      username
+    }
+  })
+}
+
+/**
+ * 更新-用户头像
+ * @param {*} avatar 头像的base64字符串
+ * @returns Promise对象
+ */
+export const updateAvatarAPI = (avatar) => {
+  return request({
+    url: '/my/update/avatar',
+    method: 'PATCH',
+    data: {
+      avatar
+    }
+  })
+}
+
+/**
+ * 更新-用户密码
+ * @param {*} param0 { old_pwd: 旧密码, new_pwd: 新密码, re_pwd: 新密码确认 }
+ * @returns Promise对象
+ */
+export const updatePwdAPI = ({ old_pwd, new_pwd, re_pwd }) => {
+  return request({
+    url: '/my/updatepwd',
+    method: 'PATCH',
+    data: {
+      old_pwd,
+      new_pwd,
+      re_pwd
+    }
+  })
+}
+
+/**
+ * 获取-新闻分类
+ * @returns Promise对象
+ */
+export const getArtCateListAPI = () => {
+  return request({
+    url: '/my/cate/list'
+  })
+}
+
+/**
+ * 增加-新闻分类
+ * @param {*} param0 { cate_name: 新闻分类名字, cate_alias: 新闻分类别名 }
+ * @returns Promise对象
+ */
+export const addArtCateAPI = ({ cate_name, cate_alias }) => {
+  return request({
+    url: '/my/cate/add',
+    method: 'POST',
+    data: {
+      cate_name,
+      cate_alias
+    }
+  })
+}
+
+/**
+ * 更新-新闻分类
+ * @param {*} param0 { id: 新闻分类id, cate_name: 新闻分类名字, cate_alias: 新闻分类别名 }
+ * @returns Promise对象
+ */
+export const updateArtCateAPI = ({ id, cate_name, cate_alias }) => {
+  return request({
+    url: '/my/cate/info',
+    method: 'PUT',
+    data: {
+      id,
+      cate_name,
+      cate_alias
+    }
+  })
+}
+
+/**
+ * 删除-新闻分类
+ * @param {*} id 要删除的-新闻分类id
+ * @returns Promise对象
+ */
+export const delArtCateAPI = (id) => {
+  return request({
+    url: '/my/cate/del',
+    method: 'DELETE',
+    params: {
+      id
+    }
+  })
+}
+
+/**
+ * 发布新闻
+ * @param {*} fd 表单对象
+ * @returns Promise对象
+ */
+export const uploadArticleAPI = (fd) => {
+  return request({
+    url: '/my/article/add',
+    method: 'POST',
+    data: fd // 参数要的是表单对象, 不能写普通对象, axios内部会判断, 如果是表单对象, 传递的请求体会设置Content-Type: form-data与后端对应
+  })
+}
+
+/**
+ * 获取新闻列表
+ * @param {*} param0 { pagenum: 当前页码数, pagesize: 当前页条数, cate_id: 新闻分类id, state: 新闻状态 }
+ * @returns Promise对象
+ */
+export const getArticleListAPI = ({ pagenum, pagesize, cate_id, state }) => {
+  return request({
+    url: '/my/article/list',
+    params: {
+      pagenum,
+      pagesize,
+      cate_id,
+      state
+    }
+  })
+}
+
+/**
+ * 获取-新闻详情
+ * @param {*} id 新闻id
+ * @returns Promise对象
+ */
+export const getArticleDetailAPI = (id) => {
+  return request({
+    url: '/my/article/info',
+    params: {
+      id
+    }
+  })
+}
+
+/**
+ * 删除-新闻
+ * @param {*} id 新闻id
+ * @returns Promise对象
+ */
+export const delArticleAPI = (id) => {
+  return request({
+    url: '/my/article/info',
+    method: 'DELETE',
+    params: {
+      id
+    }
+  })
+}
